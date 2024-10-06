4f0e57b8f3a60d12e6f567d93fac4a0e8957b165
Tom-Roman <69944327+tfr8811@users.noreply.github.com>
2024-10-06 09:02:33 -0400

    Trampoline Fix

--- Car/Assets/Scripts/Bounce.cs
+++ Car/Assets/Scripts/Bounce.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class Bounce : MonoBehaviour
 {
     [SerializeField] private int bouncePower;
+    [SerializeField] private PrometeoCarController player;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,11 +16,11 @@ public class Bounce : MonoBehaviour
 
     }
 
-    void OnCollisionEnter(Collision other)
+    void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.layer == 3)
         {
-            Rigidbody otherRigidbody = other.gameObject.GetComponent<Rigidbody>();
+            Rigidbody otherRigidbody = player.gameObject.GetComponent<Rigidbody>();
 
             // Preserve current horizontal velocities and add to the vertical component
             Vector3 currentVelocity = otherRigidbody.velocity;
